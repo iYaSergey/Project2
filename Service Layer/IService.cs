8371fc534d8b1af06c970c66b7c5a196d5f7664f
Sergey Yakimovich <ya.sergey137@gmail.com>
2020-04-26 20:41:43 +0300

    Добавьте файлы проекта.

---
+++ Service Layer/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data_Layer;
+using Data_Access_Layer;
+
+namespace Service_Layer
+{
+    public interface IService
+    {
+    }
+}
