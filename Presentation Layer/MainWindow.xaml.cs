fded73da5eb183089810e00b2b926f3b6de17017
Sergey <ya.sergey137@gmail.com>
2020-05-10 21:32:33 +0300

    GMapControl added

--- Presentation Layer/MainWindow.xaml.cs
+++ Presentation Layer/MainWindow.xaml.cs
@@ -1,20 +1,41 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Net;
 
 using Service_Layer;
 using Data_Layer;
+
+using GMap.NET.WindowsPresentation;
+using GMap.NET.MapProviders;
 
 namespace Presentation_Layer
 {
     public partial class MainWindow : Window
     {
-        string path;
-
         public readonly IService service = new Service();
         public MainWindow()
         {
+            GMapProvider.WebProxy = WebRequest.GetSystemWebProxy();
+            GMapProvider.WebProxy.Credentials = CredentialCache.DefaultNetworkCredentials;
             InitializeComponent();
             //service.ParseTweets(path);
 
+        }
+        private void MapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            // choose your provider here
+            MapView.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            MapView.MinZoom = 2;
+            MapView.MaxZoom = 17;
+            // whole world zoom
+            MapView.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            MapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            MapView.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            MapView.DragButton = MouseButton.Left;
         }
     }
 }
